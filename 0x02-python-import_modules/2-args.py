2cc9efbb417eadbb34de0d850e8fd5af1516991c
Whitneyakinyi2023 <whitneyakinyi732gmail.com>
2024-01-03 22:46:33 -0800

    a program that prints the number of and the list of its arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    w = len(argv) -1 
+    if w < 1 :
+        print ("{} arguments:".format(w))
+    elif w == 1:
+        print("{} argument:".format(w))
+    else:
+        print("{} arguments:".format(w))
+
+    for x in range(w):
+        print("{}".format(argv[x + 1]))
