49e277019a81fcccc079a7919593970570790b1e
Whitneyakinyi2023 <whitneyakinyi732gmail.com>
2024-01-23 03:13:04 -0800

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
+"""Class allocation for a square"""
+
+
 class Square:
+    """Method"""
     def __init__(self, size=0):
         self.__size = size
+        """Area of a square"""
 
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
     def area(self):
         return (self.__size ** 2)
