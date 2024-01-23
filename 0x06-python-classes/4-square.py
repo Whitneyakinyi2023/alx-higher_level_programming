14a4168aae9b8094c5f9fbdc2aa18116d6371ab2
Whitneyakinyi2023 <whitneyakinyi732gmail.com>
2024-01-23 03:16:26 -0800

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""Square class allocation"""
+
 class Square:
+    """method"""
     def __init__(self, size=0):
         self.__size =size
     def size(self):
@@ -11,5 +14,6 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
             return (self.__size ** 2)
