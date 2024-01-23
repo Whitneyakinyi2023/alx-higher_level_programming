b140f89b20d59e2eda5994b7f0bc8585f7c05d14
Whitneyakinyi2023 <whitneyakinyi732gmail.com>
2024-01-23 02:59:15 -0800

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""Square function"""
 
 class Square:
-    pass
+    """method"""
+    def __init__(self, size):
+        """The constructor method (__init__) initializes the Square instance.
+        '__size' attribute is prefixed with '__' to make it private"""
+        self.__size = size
