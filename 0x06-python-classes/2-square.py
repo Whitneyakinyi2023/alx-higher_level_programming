97d7af1e701debf58142e7938133958b4e5d5355
Whitneyakinyi2023 <whitneyakinyi732gmail.com>
2024-01-23 03:03:39 -0800

    Task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""Square size validation"""
+
+
 class Square:
     def __init__(self, size=0):
         self.__size = size
+
+        """Error handling in size validation"""
 
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-
