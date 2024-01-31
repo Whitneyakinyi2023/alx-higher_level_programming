944e4c69a8f6bc2f4e05d970741401bb4f42a335
Whitneyakinyi2023 <whitneyakinyi732gmail.com>
2024-01-31 05:05:42 -0800

    area and perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -5,32 +5,40 @@
 class Rectangle:
     """Square defined"""
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        """iClass"""
+
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
+        """Retrieva of required width"""
         return (self.__width)
 
     @width.setter
     def width(self, value):
-        if type(value) is not int:
+        """Width property setter"""
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        else:
+            self.__width = value
 
     @property
     def height(self):
+        """Retieval of required height"""
         return (self.__height)
 
     @height.setter
     def height(self, value):
-        if type(value) is not int:
+        """Height property setter"""
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        else:
+            self.__height = value
 
     def area(self):
         return (self.__height * self.__width)
