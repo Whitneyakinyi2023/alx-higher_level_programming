7cf3b48e8077c2afde428fc5cd196cc3b403e40c
Whitneyakinyi2023 <whitneyakinyi732gmail.com>
2024-03-08 00:37:59 -0800

    task 0

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import div, add, sub, mul
-    b = 5
     a = 10
+    b = 5
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
