6631463ff9aefb0ab00bf73f89825cb454a82fe8
Whitneyakinyi2023 <whitneyakinyi732gmail.com>
2024-03-08 01:04:21 -0800

    command line argument handling

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    w = len(argv) -1 
-    if w < 1 :
-        print ("{} arguments:".format(w))
+    w = len(argv) - 1
+    if w < 1:
+        print("{} arguments:".format(w))
     elif w == 1:
         print("{} argument:".format(w))
     else:
