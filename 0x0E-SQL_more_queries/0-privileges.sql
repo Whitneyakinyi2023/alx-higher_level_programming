58d26c342a2581d1309122db57837e403de76a49
Whitneyakinyi2023 <whitneyakinyi732gmail.com>
2024-03-13 03:50:46 -0700

    List priviledges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
 -- Script that lists all privileges of the MySQL users 
-GRANT ALL
-ON *.*
-TO user_0d_1@localhost, user_0d_2@localhost;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
