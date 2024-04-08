2d9074b508d95c1677968ec21413e79c0787e848
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-04-09 00:39:15 +0300

    function taht ivc=rements and calls a function

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+module.exports = {
+    addMeMaybe: function (number, theFunction) {
+        const incrementedNumber = number + 1; // Increment the number
+        theFunction(incrementedNumber); // Call the provided function with the incremented value
+    }
+};
