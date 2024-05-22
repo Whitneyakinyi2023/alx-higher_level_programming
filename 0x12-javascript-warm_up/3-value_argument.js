1aadf0bf4bedf36b6b185f70a63b33632e0847ac
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-05-22 11:31:55 +0300

    using undefined to replace the length function

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-if (typeof process.argv[2] === undefined) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
   console.log(args.join(', '));
