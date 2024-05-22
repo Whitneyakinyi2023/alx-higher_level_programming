c3db14a6c071509dd4e9b5065b7c9a229eb8c5f5
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-05-22 12:37:20 +0300

    wrting to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const material = process.argv[3];
+
+// How to write the file and handle errors
+function writeFile(args) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(filePath, material, 'utf8', (err, data) => {
+            if (err) {
+                reject(err); // Reject the promise with the error object
+            } else {
+                resolve(data); // Resolve the promise with the file content
+            }
+        });
+    });
+}
+
+// Read the file
+writeFile(filePath, material)
+    .catch(error => {
+        console.error(error); // Print the error object on error
+    });
