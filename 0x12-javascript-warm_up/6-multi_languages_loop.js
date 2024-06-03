ae072460eff4db8c0ab4190a9c74b45c75f0134a
Whitneyakinyi2023 <whitneyologi@DESKTOP-53JJO2L>
2024-06-03 12:55:00 +0300

    a script that prints x times “C is fun”

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,16 @@
 #!/usr/bin/node
-const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+// Retrieval of  the command-line argument
+const arg = process.argv[2];
 
-for (let i = 0; i < languages.length; i++) {
-  console.log(languages[i]);
+// Check if the argument is missing or not a positive integer
+if (!arg || isNaN(arg)) {
+    console.log('Missing number of occurrences');
+} else {
+    // Convert the argument to a positive integer
+    const numOccurrences = parseInt(arg);
+
+    // Print "C is fun" the specified number of times
+    for (let i = 0; i < numOccurrences; i++) {
+        console.log('C is fun');
+    }
 }
