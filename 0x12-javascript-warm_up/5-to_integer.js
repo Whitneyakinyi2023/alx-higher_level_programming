3b83fd25f76a210dfa780163ed2c0854a2f97984
Whitneyakinyi2023 <whitneyologi@DESKTOP-53JJO2L>
2024-06-03 12:37:12 +0300

    parseInt(args) attempts to convert the first argument to an integer.

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,9 +5,7 @@ const args = process.argv.slice(2);
 const number = parseInt(args[0]);
 
 if (isNaN(number)) {
-    console.log('Not a number');
+  console.log('Not a number');
 } else {
-    console.log(`My number: ${number}`);
+  console.log(`My number: ${number}`);
 }
-
-
