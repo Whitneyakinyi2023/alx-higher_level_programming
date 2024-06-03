39494a49c46892ee714f90ce5d320094c482072e
Whitneyakinyi2023 <whitneyologi@DESKTOP-53JJO2L>
2024-06-03 13:04:42 +0300

    fixed conflict issues

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,16 +1,6 @@
 #!/usr/bin/node
-// Retrieval of  the command-line argument
-const arg = process.argv[2];
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-// Check if the argument is missing or not a positive integer
-if (!arg || isNaN(arg)) {
-    console.log('Missing number of occurrences');
-} else {
-    // Convert the argument to a positive integer
-    const numOccurrences = parseInt(arg);
-
-    // Print "C is fun" the specified number of times
-    for (let i = 0; i < numOccurrences; i++) {
-        console.log('C is fun');
-    }
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
 }
---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// Retrieval of  the command-line argument
+const arg = process.argv[2];
+
+// Check if the argument is missing or not a positive integer
+if (!arg || isNaN(arg)) {
+  console.log('Missing number of occurrences');
+} else {
+  // Convert the argument to a positive integer
+  const numOccurrences = parseInt(arg);
+
+  // Print "C is fun" the specified number of times
+  for (let i = 0; i < numOccurrences; i++) {
+    console.log('C is fun');
+  }
+}
