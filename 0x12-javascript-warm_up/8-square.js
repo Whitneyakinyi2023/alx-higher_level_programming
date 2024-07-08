a7e4f5ec843e1e349463713cb7370493415cfdb6
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-07-08 12:18:25 +0300

    sscripting in JS square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Retrieval of the command-line argument
+const arg = process.argv[2];
+
+// Check if the size of square is defined and can be converted to a positive integer
+const size = parseInt(arg, 10);
+if (isNaN(size) || size <= 0) {
+    console.log('Missing size');
+} else {
+    // Print the square using the character 'X'
+    for (let i = 0; i < size; i++) {
+        console.log('X'.repeat(size));
+    }
+}
