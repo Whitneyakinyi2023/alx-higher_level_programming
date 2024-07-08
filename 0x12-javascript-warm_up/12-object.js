ec190b0744d88226a28ded549ab67bd21a70540b
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-07-08 13:07:17 +0300

    updating properties of constants

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+/*
+answer to modidify the myObject value */
+myObject.value = 89;
+console.log(myObject);
