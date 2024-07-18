9df7a9e6983b166fd172f93c47a558f2a156d248
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-07-18 12:48:21 +0300

    Addding condition to the constructor

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // Define a class Rectangle
 class Rectangle {
-    constructor(w, h) {
-        if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
-            this.width = w;
-            this.height = h;
-        }
+  constructor (w, h) {
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+      this.width = w;
+      this.height = h;
     }
+  }
 }
 
 // Export the Rectangle class to be used in other files
