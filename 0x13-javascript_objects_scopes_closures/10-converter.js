8a5deac3a1f78a5a54a8d149cebaf1f3c9081fd6
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-07-18 14:30:05 +0300

    simple

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,23 +1,5 @@
 #!/usr/bin/node
-// Define a function converter that returns another function
-exports.converter = function (base) {
-  // Define an inner function that performs the conversion recursively
-  function convert (number) {
-    // Base case: if number is 0, return an empty string
-    if (number === 0) {
-      return '';
-    } else {
-      // Calculate remainder
-      let remainder = number % base;
-      // Convert remainder to string if greater than 9
-      if (remainder >= 10) {
-        remainder = String.fromCharCode(remainder - 10 + 'A'.charCodeAt(0));
-      }
-      // Recursive case: convert the quotient and append the remainder
-      return convert(Math.floor(number / base)) + String(remainder);
-    }
-  }
 
-  // Return the inner function to perform the conversion
-  return convert;
+exports.converter = function (base) {
+    return x => x.toString(base);
 };
